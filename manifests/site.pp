ed6552b644d97769d2388fb5bb53dd9abb3a9a4a
Greg Larkin <greg.larkin@puppet.com>
2016-09-22 17:20:36 -0700

    Added support for early introduction to roles and profiles

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,5 +42,5 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Hello, my name is ${::hostname}": }
+  include role::classroom
 }
