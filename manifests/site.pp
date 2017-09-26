1b1f4da8f193e791c2f1534e82f04c0ab2ae5794
AlexinCT <32306146+AlexinCT@users.noreply.github.com>
2017-09-26 13:01:26 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,7 @@
 ## site.pp ##
 
+# Another one bites the dust.
+
 # This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
 # used when an agent connects to a master and asks for an updated configuration.
 #
