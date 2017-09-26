ceaa67edde60a5f320bc2cec300b53d3145c67dc
martybaj <32306188+martybaj@users.noreply.github.com>
2017-09-26 15:34:41 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -55,8 +55,12 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-exec { "cowsay ' Welcome to ${: : fqdn}! ' > /etc/motd":path => ' /usr/bin: /usr/local/bin' ,creates => ' /etc/motd' ,}  
-  file { '/etc/motd':
+  
+exec { "cowsay ' Welcome to ${: : fqdn}! ' > /etc/motd":
+ path => ' /usr/local/bin' ,
+ creates => ' /etc/motd' , 
+
+file { '/etc/motd':
     ensure => file,
     content => 'I learn Puppet!',
   }
