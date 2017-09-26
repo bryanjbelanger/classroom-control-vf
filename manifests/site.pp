448b615ab3fd1c04497a7b3f0138cb88db001b29
Benjamin Ritter <britter@travelers.com>
2017-09-26 13:10:02 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 ## site.pp ##
 
-# Another one bites the dust.
+# Stop it!
 
 # This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
 # used when an agent connects to a master and asks for an updated configuration.
