0fe6fe24b4b62796da2ec0c3064e0e6c4154f73f
nsyphers <32306141+nsyphers@users.noreply.github.com>
2017-09-26 14:32:08 -0400

    Manage /etc/motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,4 +44,12 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  
+  file { '/etc/motd':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    content => "Learning about Puppet\n",
+  }
 }
