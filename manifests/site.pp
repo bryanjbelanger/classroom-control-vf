600409e5f97fcfde6bfa733e97e1a5aee5283a41
badhika2 <32306200+badhika2@users.noreply.github.com>
2017-09-26 15:05:45 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -49,4 +49,7 @@ node default {
     owner => 'root',
     content => "This is interesting, \n",
     }
+  exec { "cowsay 'Welcome to $(::fqdn}!' > /etc/motd":
+    creates => 'etc/motd',
+        }
 }
