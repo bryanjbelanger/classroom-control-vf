ad971c6d648a71fa983abd06e11c7a2b7ed7ea1f
Vishy <viswanadhk@yahoo.com>
2017-09-26 15:07:07 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,13 +43,7 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-  file { '/etc/motd':
-ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0644',
-content => "Today I learned what it means to manage state using Puppet.\n",
-}
+  
 exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
 path => '/usr/bin:/usr/local/bin',
 creates => '/etc/motd',
