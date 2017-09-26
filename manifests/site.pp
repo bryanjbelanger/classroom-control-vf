fa73d9d19bfc698455df9655599e808941e434b2
Vishy <viswanadhk@yahoo.com>
2017-09-26 14:31:19 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,11 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  file { '/etc/motd':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+content => "Today I learned what it means to manage state using Puppet.\n",
+}
 }
