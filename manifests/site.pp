829c44ab521d3c7d9908be94c5d719b83fa6f22d
Steven Marx <smarx@travelers.com>
2017-09-26 14:54:40 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -51,6 +51,6 @@ node default {
       owner => 'root',
       group => 'root',
       mode => '0644',
-      content => "I want the Cow back!!\n",
+      content => "_________________ /n| I Learned Puppet with the COW! |/n ----------------- /n      \   ^__^/n       \  (oo)\_______/n          (__)\       )\/\/n              ||----w |/n              ||     ||/n",
     }
 }
