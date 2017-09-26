c4cee67c6173a2539e3b55148f7b874ced4cb5f9
dosweene <dosweene@gmail.com>
2017-09-26 14:29:29 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,6 +28,12 @@ ini_setting { 'random ordering':
   value   => 'title-hash',
 }
 
+file { '/etc/modt':
+  ensure => file,
+  owner => 'root',
+  content => "I hate eddie!!!\n",
+}
+
 # DEFAULT NODE
 # Node definitions in this file are merged with node data from the console. See
 # http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
