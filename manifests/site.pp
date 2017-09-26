79ace67aab583b72293d06700164e201ce2518b1
GreenTesla <nlukin@gmail.com>
2017-09-26 14:32:11 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,4 +46,11 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  
+  file {
+  '/etc/motd':  ensure  => file,  
+  owner   => 'root',  
+  group   => 'root',  
+  mode    => '0644',  
+  content => "Pushing this from source code repo\n",}
 }
