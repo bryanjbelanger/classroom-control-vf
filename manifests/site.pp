b90cc97aa19124546fa276ba4e9d291b42a1919f
GreenTesla <nlukin@gmail.com>
2017-09-27 10:38:55 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,7 +45,10 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include role::classroom
+  
+  
+  include ::users
+  
   
   file { '/etc/motd':
    ensure => file,
