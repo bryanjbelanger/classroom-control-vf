5083e92787f2f1272dbe041b06ef8c91a521038a
badhika2 <32306200+badhika2@users.noreply.github.com>
2017-09-27 09:54:37 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,7 +1,7 @@
 class skeleton{
   file { '/etc/skel/.bashrc':
      ensure => file,
-     source => 'puppet:///modules/usr/local/bin',
+     source => 'puppet:///modules/skeleton/bashrc',
      }
   file { 'etc/skel/
     ensure => directory,
