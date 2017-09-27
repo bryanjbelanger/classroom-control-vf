4b9985e75643006f99d7bf8d527195a08d10d007
Autostructure <bbelanger@azcender.com>
2017-09-27 14:11:15 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,28 +1,25 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0664',
+  }
+
   package { 'nginx':
     ensure => present,
   }
   
   file { '/var/www':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0775',
   }
   
   file { '/var/www/index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/index.html',
   }
   
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
@@ -30,16 +27,10 @@ class nginx {
   
   file { '/etc/nginx/conf.d':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0775',
   }
   
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
