4ee20f3c81c6caf692c5703b6b29e87d7e1b7235
dosweene <dosweene@gmail.com>
2017-09-27 13:10:32 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,1 +1,11 @@
-
+class memcached {
+  package { 'memcached': 
+    ensure => present,
+  }
+  file {'/etc/sysconfig/memcached':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
+}
