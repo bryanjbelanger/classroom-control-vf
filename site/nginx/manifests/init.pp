dc490ee28629edd7a95d2122581f7e1f63493f5e
ewestdal <32306206+ewestdal@users.noreply.github.com>
2017-09-27 10:20:36 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,15 @@
-blah
+class nginx {
+  # create a directory
+  file { '/etc/skel':
+    ensure => 'directory',
+  }
+
+  # a fuller example, including permissions and ownership
+  file { '/etc/skel/.bashrc':
+    ensure => 'file',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
+    source => 'puppet:///modules/skeleton/.bashrc'
+  }
+}
