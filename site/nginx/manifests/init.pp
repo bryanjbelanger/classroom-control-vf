6be39a822650802b7c065d6cd27c46bd50cb5217
oldfark1 <32306320+oldfark1@users.noreply.github.com>
2017-09-27 14:08:39 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,46 +1,34 @@
 class nginx {
-package { 'nginx':
-ensure => present,
-}
-file { '/var/www':
-ensure => directory,
-owner => 'root',
-group => 'root',
-mode => '0775',
-}
-file { '/var/www/index.html':
-ensure => file,
+File {
 owner => 'root',
 group => 'root',
 mode => '0664',
+}
+package { 'nginx':
+ensure => present,
+}
+file { [ '/var/www', '/etc/nginx/conf.d' ]:
+ensure => directory,
+}
+file { '/var/www/index.html':
+ensure => file,
 source => 'puppet:///modules/nginx/index.html',
 }
 file { '/etc/nginx/nginx.conf':
 ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0664',
 source => 'puppet:///modules/nginx/nginx.conf',
 require => Package['nginx'],
 notify => Service['nginx'],
 }
-file { '/etc/nginx/conf.d':
-ensure => directory,
-owner => 'root',
-group => 'root',
-mode => '0775',
-}
 file { '/etc/nginx/conf.d/default.conf':
 ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0664',
 source => 'puppet:///modules/nginx/default.conf',
-require => Package['nginx'],
 notify => Service['nginx'],
+require => Package['nginx'],
 }
 service { 'nginx':
 ensure => running,
 enable => true,
 }
 }
+
