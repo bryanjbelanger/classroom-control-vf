4275ead12474598b0a28f615d1b5b38bd593481f
AlexinCT <32306146+AlexinCT@users.noreply.github.com>
2017-09-27 10:26:30 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,24 @@
 #Add content
+
+class nginx {
+
+  file { 'document':
+    ensure => directory,
+  }
+  
+  file {'document/index.html':
+    ensure = > file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  
+  file {'document/default.conf':
+    ensure = > file,
+    source => 'puppet:///modules/nginx/default.conf',
+  }
+ 
+   file {'document/nginx.conf':
+    ensure = > file,
+    source => 'puppet:///modules/nginx/nginx.conf',
+  }
+  
+}
