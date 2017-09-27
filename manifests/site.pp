23c3e8ad9cb8b09542c5d5bcbfe9e29f7929977d
JohnnyB-CT <32306220+JohnnyB-CT@users.noreply.github.com>
2017-09-27 09:23:10 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,7 +45,7 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-  include class
+  include users
    
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
     path => '/usr/bin:/usr/local/bin',
