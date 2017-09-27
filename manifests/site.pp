8b84903162dd42162eee7ace6c52978a0314552e
Steven Marx <smarx@travelers.com>
2017-09-27 10:02:51 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,11 +46,9 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   
-  file { '/etc/motd':
-      ensure => file,
-      owner => 'root',
-      group => 'root',
-      mode => '0644',
-      content => "I Learned Puppet with the COW!\n",
-    }
+  exec { "cowsay ' Welcome to ${: : fqdn}! ' > /etc/motd":
+    path => ' /usr/local/bin' ,
+    creates => ' /etc/motd' , 
+
+include users
 }
