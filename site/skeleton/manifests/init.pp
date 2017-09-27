3af5ea5ff57faa21f30cab34c81a0f6a608f40d5
dpoppel <32306235+dpoppel@users.noreply.github.com>
2017-09-27 09:52:43 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,1 +1,16 @@
 
+class skeleton {
+  file { '/etc/skel':
+  ensure => directory,
+  owner => 'root',
+  group => 'root',
+  mode => '0755',
+  }
+  file { '/etc/skel/.bashrc':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/skeleton/bashrc',
+  }
+}
