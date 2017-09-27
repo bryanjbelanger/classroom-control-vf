123dba0af391467362d07c8322aea49debf5279e
nsyphers <32306141+nsyphers@users.noreply.github.com>
2017-09-27 11:50:09 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,11 +39,11 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
+#node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include role::classroom
+  #include role::classroom
   
   #file { '/etc/motd':
   #  ensure => file,
@@ -60,6 +60,6 @@ node default {
   #  creates ==> '/etc/motd',
   #  path ==> '/usr/local/bin',
   #}
-  include users 
-  include nginx
-}
+  #include users 
+  #include nginx
+#}
