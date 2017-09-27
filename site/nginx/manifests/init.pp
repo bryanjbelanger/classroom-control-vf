d7fff55b4bb7e44cde12ae709b4cf0153a6a6cbd
christopherwatkin <32306314+christopherwatkin@users.noreply.github.com>
2017-09-27 14:08:24 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,28 +1,26 @@
 class nginx {
+
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
+
   package {'nginx':
     ensure => present,
   }
   
   file {'/var/www':
     ensure => 'directory',
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
   }
   
   file {'/var/www/index.html':
     ensure => 'file',
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
     source => 'puppet:///modules/nginx/index.html',
   }
   
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
@@ -30,16 +28,10 @@ class nginx {
 
   file { '/etc/nginx/conf.d':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0775',
   }
 
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
