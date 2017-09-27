0ff5a6dfafd59d168e06fb41e1cb0b2ffd9f9125
dosweene <dosweene@gmail.com>
2017-09-27 10:51:33 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -13,26 +13,26 @@ class nginx {
     mode => '0755',
     source => 'puppet:///modules/nginx/index.html',
   }
-  file {' /etc/nginx':
+  file {'/etc/nginx':
     ensure => directory,
     owner => 'root',
     group => 'root',
     mode => '0755',
   }
-  file { ' /etc/nginx/nginx.conf':
+  file { '/etc/nginx/nginx.conf':
     ensure => file,
     owner => 'root',
     group => 'root',
     mode => '0755',
     source => 'puppet:///modules/nginx/nginx.conf',
   }
-  file { ' /etc/nginx/conf.d':
+  file { '/etc/nginx/conf.d':
     ensure => directory,
     owner => 'root',
     group => 'root',
     mode => '0755',
   }  
-  file { ' /etc/nginx/conf.d/default.conf':
+  file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
     owner => 'root',
     group => 'root',
