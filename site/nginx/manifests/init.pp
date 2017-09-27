caaa0daaedaddef362d9d79c2116f4650a06d134
ewestdal <32306206+ewestdal@users.noreply.github.com>
2017-09-27 14:10:33 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,49 +1,42 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+  }
+  
   #ensure package is present
   package { 'nginx':
     ensure => present,
   }
+  
   # create a directory
-  file { '/var/www':
+  file { [ '/var/www', '/etc/nginx/conf.d' ]:
     ensure => 'directory',
-    owner => 'root',
-    group => 'root',
-    mode => '0775',
   }
+  
   # ensure index.html exists
   file { '/var/www/index.html':
     ensure => 'file',
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/index.html',
   }
+  
   # a fuller example, including permissions and ownership
   file { '/etc/nginx/nginx.conf':
     ensure => 'file',
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
   }
-    file { '/etc/nginx/conf.d':
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0775',
-  }
+
   # a fuller example, including permissions and ownership
   file { '/etc/nginx/conf.d/default.conf':
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     ensure => 'file',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
   }
+  
   service { 'nginx':
     ensure => running,
     enable => true,
