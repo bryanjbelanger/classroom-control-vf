037c0b278cb4264d5f8d2e6b3a2ed2215aacada7
JohnnyB-CT <32306220+JohnnyB-CT@users.noreply.github.com>
2017-09-27 14:08:56 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,33 +2,25 @@ class nginx {
   package { 'nginx':
     ensure => present,
   }
-  file { '/var/www':
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-  }
-  file { '/var/www/index.html':
-    ensure => file,
+  File {
     owner => 'root',
     group => 'root',
     mode => '0664',
+    ensure => file,
+  }
+  file { '/var/www':
+    ensure => directory,
+  }
+  
+  file { '/var/www/index.html':
     source => 'puppet:///modules/nginx/index.html',
   }
   file { '/etc/nginx/nginx.conf':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
   }
   file { '/etc/nginx/conf.d/default.conf':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
