487079cd197953b463fd8a4d2c8a1664ad03db08
dpoppel <32306235+dpoppel@users.noreply.github.com>
2017-09-27 14:41:52 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,16 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+node default {
+  # This is where you can declare classes for all nodes.
+  # Example:
+  # class { 'my_class': }
+  if $::virtual != 'physical' {
+  $vmname = capitalize($::virtual)
+  notify { "This is a ${vmname} virtual machine.": }
+  }
+}
+
 ##node default {    
   # This is where you can declare classes for all nodes.
   # Example:    #   class { 'my_class': }
