97a857fffc24ea88cb3cd78715911834151d1423
Steven Marx <smarx@travelers.com>
2017-09-27 11:29:55 -0400

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,8 @@
 
+class users {
+user { 'fundamentals':
+ensure => present,
+# password => 'puppet8#labs', # Windows requires a plain text password
+# groups => ['Users'], # Display in Windows Control Panel
+}
+}
