3a8cf760ec8e2b95ae17311a5493d6c8f48d6310
ewestdal <32306206+ewestdal@users.noreply.github.com>
2017-09-27 10:32:36 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,15 +1,37 @@
 class nginx {
+  #ensure package is present
+  package { 'nginx':
+    ensure => present,
+  }
   # create a directory
-  file { '/etc/skel':
+  file { '/var/www':
     ensure => 'directory',
   }
 
   # a fuller example, including permissions and ownership
-  file { '/etc/skel/.bashrc':
+  file { '/etc/nginx/nginx.conf':
     ensure => 'file',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0664',
-    source => 'puppet:///modules/skeleton/.bashrc'
+    source => 'puppet:///modules/nginx/nginx.conf'
+  }
+  
+  # a fuller example, including permissions and ownership
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => 'file',
+    source => 'puppet:///modules/nginx/nginx.conf'
+  }
+    # a fuller example, including permissions and ownership
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => 'file',
+    source => 'puppet:///modules/nginx/index.html'
+  }
+  file { '/etc/nginx/conf.d':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
   }
 }
