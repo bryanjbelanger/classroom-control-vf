a0dd4f1327cbab1eb5bb8960eebf1701001dc29c
ewestdal <32306206+ewestdal@users.noreply.github.com>
2017-09-27 10:02:35 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -10,6 +10,6 @@ class skeleton {
     owner  => 'root',
     group  => 'root',
     mode   => '0664',
-    source => 'puppet:///modules/skeleton/bashrc
+    source => 'puppet:///modules/skeleton/bashrc'
   }
 }
