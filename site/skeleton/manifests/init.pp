fba2aa46c7be80cc1b6b61d41e9bd2207bf612cb
AlexinCT <32306146+AlexinCT@users.noreply.github.com>
2017-09-27 10:38:34 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,12 +1,18 @@
 # Create a directory so we can then copy file to it. If directory doesn't exist the file copy would fail.
 class skeleton {
 
-  file { 'etc/skel':
+  file { '/etc/skel':
     ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
   }
   
-  file {'etc/skel/.bashrc':
-    ensure = > file,
-    source => 'puppet:///modules/sudo/baschrc',
+  file { '/etc/skel/.bashrc':
+      ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/skeleton/bashrc',
   }
 }
