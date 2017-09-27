5121777abb7bd9077fc7d64f9379c4229e76cfdc
Autostructure <bbelanger@azcender.com>
2017-09-27 13:05:48 -0400

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,16 @@
+class memcached {
+  package { 'memcached:
+    ensure => present,
+  }
+  
+  file { '/etc/sysconfig/memcached':
+    ensure => file,
+    require => Package['memcached'],
+  }
+  
+  service { 'memcached':
+    ensure => running,
+    enabled => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
+}
