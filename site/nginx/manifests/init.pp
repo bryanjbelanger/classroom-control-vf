0c28fb2a2e789f79a709cf52b643814c682d3137
nsyphers <32306141+nsyphers@users.noreply.github.com>
2017-09-27 15:53:13 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,35 @@
 class nginx {
+  case $facts['os']['family'] {
+  'redhat','debian' : {
+  $package = 'nginx'
+  $owner = 'root'
+  $group = 'root'
+  $docroot = '/var/www'
+  $confdir = '/etc/nginx'
+  $logdir = '/var/log/nginx'
+  }
+
+  'windows' : {
+    $package = 'nginx-service'
+    $owner = 'Administrator'
+    $group = 'Administrators'
+    $docroot = 'C:/ProgramData/nginx/html'
+    $confdir = 'C:/ProgramData/nginx'
+    $logdir = 'C:/ProgramData/nginx/logs'
+   }
+
+  default : {
+    fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+  }
+  }
+
+# user the service will run as. Used in the nginx.conf.epp template
+
+$user = $facts['os']['family'] ? {
+  'redhat' => 'nginx',
+  'debian' => 'www-data',
+  'windows' => 'nobody',
+}
 
   File {
    owner => 'root',
