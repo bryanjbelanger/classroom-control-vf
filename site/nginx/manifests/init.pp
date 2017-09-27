b273cc086cd9c7a0895a8d5a9aa794d7ffcbe66e
smmccue <32306339+smmccue@users.noreply.github.com>
2017-09-27 10:48:39 -0400

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,48 @@
+class nginx {
+package { 'nginx':
+ensure => present,
+}
+file { '/var/www':
+ensure => directory,
+owner => 'root',
+group => 'root',
+mode => '0775',
+}
+file { '/var/www/index.html':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0664',
+source => 'puppet:///modules/nginx/index.html',
+}
+file { '/etc/nginx/nginx.conf':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0664',
+source => 'puppet:///modules/nginx/nginx.conf',
+require => Package['nginx'],
+notify => Service['nginx'],
+}
+file { '/etc/nginx/conf.d':
+ensure => directory,
+owner => 'root',
+group => 'root',
+mode => '0775',
+}
+file { '/etc/nginx/conf.d/default.conf':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0664',
+source => 'puppet:///modules/nginx/default.conf',
+require => Package['nginx'],
+notify => Service['nginx'],
+}
+service { 'nginx':
+ensure => running,
+enable => true,
+}
+}
+Homework 8.4: Proposed Solution
+VirtualFundamentals
