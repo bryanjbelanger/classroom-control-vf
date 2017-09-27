00341de75184fcc7657ac8b2ba2e429d8af0757a
badhika2 <32306200+badhika2@users.noreply.github.com>
2017-09-27 10:44:01 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,6 +1,49 @@
 class nignx {
-  ensure => nignx,
-  ensure => '/site/nginx/files/index.html',
-  ensure => 'puppet://module/site/nginx',
+  package { 'nginx':
+    ensure => present,
+    }
+  file { 'var/www':
+    ensure => directory,
+    owner => 'root',
+    group -=> 'root',
+    mode => '0775',
+    }
+  file { 'var/www/index.html':
+    ensure => file, 
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet://modules/nginx/index.html',
+    }
+  file { 'etc/nginx/nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet://modules/nginx.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+    }
+  file { 'etc/nginx/conf.d':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+    }
+  file { 'etc/nginx/conf.d/default.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify = Service['nginx'],
+    }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    }
+    
+    
   
 }
