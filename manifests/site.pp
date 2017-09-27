0c485021f5b195dde5bca1f6027d9a34b0c98aff
AlexinCT <32306146+AlexinCT@users.noreply.github.com>
2017-09-27 14:36:01 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -41,26 +41,11 @@ ini_setting { 'random ordering':
 # specified in the console for that node.
 
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-  #include role::classroom
-  
-  include ::users
-
-	# MOTD FILE
-	# Create a file with MOTD entry
-
-	file { '/etc/motd':
-	ensure => file,
-	owner => 'root',
-	group => 'root',
-	mode => '0644',
-	content => "Can a brotha get a table dance\n",
-	}
-
-	exec {"cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
-		creates => '/etc/motd',
-		path => '/usr/local/bin',
+	# This is where you can declare classes for all nodes.
+	# Example:
+	# class { 'my_class': }
+	if $::virtual != 'physical' {
+		$vmname = capitalize($::virtual)
+		notify { "This is a ${vmname} virtual machine.": }
 	}
 }
