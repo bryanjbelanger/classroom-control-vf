419d0ba0924b9881ffa676c16ac92da920c73be5
kcyr <32306311+kcyr@users.noreply.github.com>
2017-09-27 14:43:36 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,10 +38,13 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-#node default {
+node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-#  include role::classroom
-#}
-#include memcached
+  include role::classroom
+  if $::virtual != 'physical' {
+      $vmname = capitalize($::virtual)
+      notify { "This is a ${vmname} virtual machine.": }
+  }
+}
