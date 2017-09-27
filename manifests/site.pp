1454d0be2af83ebd46dbb90e75bf3927b4b7157f
oldfark1 <32306320+oldfark1@users.noreply.github.com>
2017-09-27 11:35:55 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -40,11 +40,11 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
+#node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include role::classroom
+  #include role::classroom
   
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
       path => '/usr/local/bin',
