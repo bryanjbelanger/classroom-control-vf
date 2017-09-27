053d2b6846aff6b669b6ab2c5d30ba69d025a745
JohnnyB-CT <32306220+JohnnyB-CT@users.noreply.github.com>
2017-09-27 11:36:01 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -40,7 +40,8 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
+# node default {
+node bob {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
