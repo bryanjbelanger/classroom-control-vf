d142e7e9e9977286bd631005b5d6403331371a6c
dosweene <dosweene@gmail.com>
2017-09-27 10:31:48 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,16 @@
 
+class nginx {
+  file {'/var/www':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
+  file { '/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+    source => 'puppet:///modules/nginx/index.html',
+  }  
+}
