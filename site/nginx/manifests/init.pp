65fd2fc57ee672a3eb4bc57f55184de971a70d9e
ewestdal <32306206+ewestdal@users.noreply.github.com>
2017-09-27 10:39:17 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -6,29 +6,43 @@ class nginx {
   # create a directory
   file { '/var/www':
     ensure => 'directory',
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
   }
-
+  # ensure index.html exists
+  file { '/var/www/index.html':
+    ensure => 'file',
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/index.html'
+  }
   # a fuller example, including permissions and ownership
   file { '/etc/nginx/nginx.conf':
     ensure => 'file',
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf'
+    require => Package['nginx'],
+    notify => Service['nginx'],
   }
-  
-  # a fuller example, including permissions and ownership
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure => 'file',
-    source => 'puppet:///modules/nginx/nginx.conf'
-  }
-    # a fuller example, including permissions and ownership
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure => 'file',
-    source => 'puppet:///modules/nginx/index.html'
-  }
-  file { '/etc/nginx/conf.d':
+    file { '/etc/nginx/conf.d':
     ensure => directory,
     owner => 'root',
     group => 'root',
     mode => '0775',
+  }
+  # a fuller example, including permissions and ownership
+  file { '/etc/nginx/conf.d/default.conf':
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    ensure => 'file',
+    source => 'puppet:///modules/nginx/nginx.conf'
+    require => Package['nginx'],
+    notify => Service['nginx'],
   }
   service { 'nginx':
     ensure => running,
