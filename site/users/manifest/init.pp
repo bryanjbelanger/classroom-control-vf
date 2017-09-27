5679edd26d310d51a78430210cc24a3c672310fd
GreenTesla <nlukin@gmail.com>
2017-09-27 11:41:25 -0400

    Update init.pp

--- site/users/manifest/init.pp
+++ site/users/manifest/init.pp
@@ -1,1 +1,13 @@
-test
+class users {
+
+user { 'fundamentals':
+
+ensure => present,
+
+# password => 'puppet8#labs', # Windows requires a plain text password
+
+# groups => ['Users'], # Display in Windows Control Panel
+
+}
+
+}
