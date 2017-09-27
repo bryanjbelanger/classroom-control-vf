5e748e68b6825f29a393fe06a082134699935201
oldfark1 <32306320+oldfark1@users.noreply.github.com>
2017-09-27 15:15:29 -0400

    Update init.pp

--- site/aliases/manifests/init.pp
+++ site/aliases/manifests/init.pp
@@ -1,16 +1,16 @@
 class aliases (
-    $admin = 'root',
+$admin = 'root',
 ) {
-    # uses $admin to build the aliases file
-    file { '/etc/aliases':
-        ensure  => file,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        content => epp('aliases/aliases.epp', { admin => $admin }),
-    }
-    exec { '/usr/bin/newaliases':
-        refreshonly => true,
-        subscribe   => File['/etc/aliases'],
-    }
+# uses $admin to build the aliases file
+file { '/etc/aliases':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+content => epp('aliases/aliases.epp', { admin => $admin }),
+}
+exec { '/usr/bin/newaliases':
+refreshonly => true,
+subscribe => File['/etc/aliases'],
+}
 }
