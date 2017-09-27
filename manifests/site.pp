6eea3d05f7905acd7b3e2e6eca85026f38fb19b3
ewestdal <32306206+ewestdal@users.noreply.github.com>
2017-09-27 11:33:53 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,7 +44,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include ::users
+  #include ::users
   include role::classroom
   include ::skeleton
   notify { "Hello, my name name is ${::hostname}": }
