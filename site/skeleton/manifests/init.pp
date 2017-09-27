03e83980d8941b897e86ef27b204383e8cc4e2d4
badhika2 <32306200+badhika2@users.noreply.github.com>
2017-09-27 09:51:28 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -2,9 +2,10 @@ class skeleton{
   file { '/etc/skel/.bashrc':
      ensure => file,
      content => "hi",
+     source => 'puppet:///modules/usr/local/bin',
      }
   node default {
     if [ -f /etc/bashrc ]; then        . /etc/bashrcfi
-    include .bashrc
   }
+  
 }
