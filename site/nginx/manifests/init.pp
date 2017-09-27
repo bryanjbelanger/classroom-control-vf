2cd51f45b1718aabd1fde4aaf1466ebfe7036537
dosweene <dosweene@gmail.com>
2017-09-27 14:22:02 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -19,11 +19,6 @@ class nginx {
     ensure => file,
     source => 'puppet:///modules/nginx/nginx.conf',
   }
-  file { '/etc/nginx/conf.d':
-    ensure => file,
-    require => Package['nginx'],
-    notify => Service['nginx'],
-  }  
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/default.conf',
