2ab0b9a708cc27f621fcce00d5bf7886a620f210
oldfark1 <32306320+oldfark1@users.noreply.github.com>
2017-09-27 12:57:05 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 ## site.pp ##
 
-# Mikes great Stuff
+# Eddie commit
 
 # This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
 # used when an agent connects to a master and asks for an updated configuration.
@@ -40,65 +40,26 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-#node default {
+node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  #include role::classroom
-  
-  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
-      path => '/usr/local/bin',
-      unless => '/usr/bin/test -f /etc/motd',
+  #include ::users
+  include role::classroom
+  include ::skeleton
+  notify { "Hello, my name name is ${::hostname}": }
+  #file { '/etc/motd':
+  #  ensure => file,
+  #  owner => 'root',
+  #  group => 'root',
+  #  mode => '0644',
+  #  content => "Today I learned what it means to be a puppet.\n",
+  #}
+  exec { 'cowsay':
+    command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motb",
+    path => '/usr/local/bin',
+    creates => '/etc/motd',
   }
+    
   
-  file { '/etc/motd':
-      owner => root,
-      group => root,
-      mode => '0700',
-  }
-## site.pp ##
-# This is a comment added by Kev
-# This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
-# used when an agent connects to a master and asks for an updated configuration.
-#
-# Global objects like filebuckets and resource defaults should go in this file,
-# as should the default node definition. (The default node can be omitted
-# if you use the console and don't define any other nodes in site.pp. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.)
-
-## Active Configurations ##
-
-# PRIMARY FILEBUCKET
-# This configures puppet agent and puppet inspect to back up file contents when
-# they run. The Puppet Enterprise console needs this to display file contents
-# and differences.
-
-# Disable filebucket by default for all File resources:
-File { backup => false }
-
-# Randomize enforcement order to help understand relationships
-ini_setting { 'random ordering':
-  ensure  => present,
-  path    => "${settings::confdir}/puppet.conf",
-  section => 'agent',
-  setting => 'ordering',
-  value   => 'title-hash',
 }
-
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
-
-# The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
-
-#node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-#  include role::classroom
-#}
