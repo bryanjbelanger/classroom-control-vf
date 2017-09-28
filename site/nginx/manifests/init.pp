f3072baaddb363f930dbe6ae35506add8f8fe2a6
smmccue <32306339+smmccue@users.noreply.github.com>
2017-09-28 13:36:33 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,75 +1,41 @@
 class nginx (
-  Optional[String] $root = undef, 
-  Boolean $highperf = true,
-){
-  case $facts['os']['family'] {
-    'redhat','debian' : { 
-     $package = 'nginx'
-     $owner = 'root'
-     $group = 'root'
-     #$docroot = '/var/www' 
-     $confdir = '/etc/nginx' 
-     $logdir = '/var/log/nginx'
-     $default_docroot = '/var/www'
-  }
-  'windows' : {
-     $package = 'nginx-service'
-     $owner = 'Administrator'
-     $group = 'Administrators'
-     #$docroot = 'C:/ProgramData/nginx/html' 
-     $confdir = 'C:/ProgramData/nginx' 
-     $logdir = 'C:/ProgramData/nginx/logs'
-     $default_docroot = 'C:/ProgramData/nginx/html'
-  }
-  default : {
-      fail("Module ${module_name} is not supported on ${facts['os']['family']}") 
-   }
- }
-  #if $root isn't set, then fall back to the platform default
-  $docroot = $root ? {
-    undef => $default_docroot, 
-    default => $root,
-  }
-  # user the service will run as. Used in the nginx.conf.epp template
-  $user = $facts['os']['family'] ? { 
-    'redhat' => 'nginx',
-    'debian' => 'www-data', 
-    'windows' => 'nobody',
-  }
+String $package = $nginx::params::package,
+String $owner = $nginx::params::owner,
+String $group = $nginx::params::group,
+String $docroot = $nginx::params::docroot,
+String $confdir = $nginx::params::confdir,
+String $blockdir = $nginx::params::blockdir,
+String $logdir = $nginx::params::logdir,
+String $user = $nginx::params::user,
+Boolean $highperf = $nginx::params::highperf,
+) inherits nginx::params {
+File {
+owner => $owner,
+group => $group,
+mode => '0664',
+}
+package { $package:
+ensure => present,
+}
 
-  package { $package: 
-    ensure => present,
-  }
-  File {
-    owner => $owner,
-    group => $group,
-    mode => '0644',
-  }
-  file { [ $docroot, "${confdir}/conf.d" ]: 
-    ensure => directory,
-  }
-  file { "${docroot}/index.html":
-    ensure => file,
-    source => 'puppet:///modules/nginx/index.html',
-  }
-  file { "${confdir}/nginx.conf":
-    ensure => file,
-    content => epp('nginx/nginx.conf.epp',
-     {
-       user => $user, confdir => $confdir, logdir => $logdir, 
-      }),
-    notify => Service['nginx'], 
-  }
-  file { "${confdir}/conf.d/default.conf": 
-    ensure => file,
-    content => epp('nginx/default.conf.epp',
-                   {
-                     docroot => $docroot, 
-                    }),
-    notify => Service['nginx'], 
-  }
-  service { 'nginx': 
-    ensure => running, 
-    enable => true,
-  } 
+file { "${docroot}/vhosts":
+ensure => directory,
+}
+file { "${confdir}/nginx.conf":
+ensure => file,
+content => epp('nginx/nginx.conf.epp',
+{
+user => $user,
+logdir => $logdir,
+confdir => $confdir,
+blockdir => $blockdir,
+highperf => $highperf,
+}),
+require => Package[$package],
+notify => Service['nginx'],
+}
+service { 'nginx':
+ensure => running,
+enable => true,
+}
 }
