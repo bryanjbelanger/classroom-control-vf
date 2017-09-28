3227bf5202b803b78b3242299d752feebfba51a8
oldfark1 <32306320+oldfark1@users.noreply.github.com>
2017-09-28 10:18:32 -0400

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,3 +1,8 @@
 class profile::base {
-  notify { "Hello, my name is ${::hostname}": }
+# This is where you can declare classes for all nodes.
+# Example:
+# class { 'my_class': }
+$message = lookup('message')
+notify { $message: }
 }
+
