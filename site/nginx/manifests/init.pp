d6a6560b97a2d78cdc3b78e3668ff04245db937a
Autostructure <bbelanger@azcender.com>
2017-09-28 12:09:42 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,22 +3,22 @@ class nginx(
 ) {
   case $facts['os']['family'] {
     'redhat','debian' : {
-      $package = 'nginx',
-      $owner = 'root',
-      $group = 'root',
-      # $docroot = '/var/www',
-      $confdir = '/etc/nginx',
-      $logdir = '/var/log/nginx',
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      # $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir = '/var/log/nginx'
 
       # this will be used if we don't pass in a value
       $default_docroot = '/var/www'
     }
     'windows' : {
-      $package = 'nginx-service',
-      $owner = 'Administrator',
-      $group = 'Administrators',
-      # $docroot = 'C:/ProgramData/nginx/html',
-      $confdir = 'C:/ProgramData/nginx',
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      # $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
       $logdir = 'C:/ProgramData/nginx/logs'
 
       # this will be used if we don't pass in a value
