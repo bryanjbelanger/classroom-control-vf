78baa61c5355d2a466c554ec7b5619a6bfef1d3a
christopherwatkin <32306314+christopherwatkin@users.noreply.github.com>
2017-09-28 11:11:35 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,21 +1,28 @@
-class nginx (String $root = '/var/www'
+class nginx (Optional[String] $root = undef,
+             Boolean $highperf = true,
             ){
   case $facts['os']['family'] {
     'redhat','debian' : {
       $package = 'nginx'
       $owner = 'root'
       $group = 'root'
-      $docroot = $root
+      #$docroot = '/var/www'
       $confdir = '/etc/nginx'
       $logdir = '/var/log/nginx'
+      
+      # this will be used if we don't pass in a value
+      $default_docroot = '/var/www'
     }
     'windows' : {
       $package = 'nginx-service'
       $owner = 'Administrator'
       $group = 'Administrators'
-      $docroot = 'C:/ProgramData/nginx/html'
+      #$docroot = 'C:/ProgramData/nginx/html'
       $confdir = 'C:/ProgramData/nginx'
       $logdir = 'C:/ProgramData/nginx/logs'
+      
+      # this will be used if we don't pass in a value
+      $default_docroot = 'C:/ProgramData/nginx/html'
     }
     default : {
       fail("Module ${module_name} is not supported on ${facts['os']['family']}")
@@ -29,6 +36,12 @@ class nginx (String $root = '/var/www'
     'windows' => 'nobody',
   }
 
+  # if $root isn't set, then fall back to the platform default
+  $docroot = $root ? {
+    undef => $default_docroot,
+    default => $root,
+  }
+
   File {
     owner => $owner,
     group => $group,
@@ -39,13 +52,14 @@ class nginx (String $root = '/var/www'
     ensure => present,
   }
 
-  file { [ $docroot, "${confdir}/conf.d" ]:
-    ensure => directory,
+  # docroot is either passed in or a default value
+  nginx::vhost { 'default':
+    docroot => $docroot,
+    servername => $facts['fqdn'],
   }
-
-  file { "${docroot}/index.html":
-    ensure => file,
-    source => 'puppet:///modules/nginx/index.html',
+  
+  file { "${docroot}/vhosts":
+    ensure => directory,
   }
 
   file { "${confdir}/nginx.conf":
@@ -53,19 +67,13 @@ class nginx (String $root = '/var/www'
     content => epp('nginx/nginx.conf.epp',
       {
         user => $user,
-        confdir => $confdir,
         logdir => $logdir,
+        confdir => $confdir,
+        blockdir => $blockdir,
+        highperf => $highperf,
       }),
-    notify => Service['nginx'],
-  }
-
-  file { "${confdir}/conf.d/default.conf":
-    ensure => file,
-    content => epp('nginx/default.conf.epp',
-    {
-      docroot => $docroot,
-    }),
-    notify => Service['nginx'],
+      require => Package[$package],
+      notify => Service['nginx'],
   }
 
   service { 'nginx':
