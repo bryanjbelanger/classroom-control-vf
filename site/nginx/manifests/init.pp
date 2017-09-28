737ccfd9714c65ec822d18e33bd89a77ff6ae3ad
christopherwatkin <32306314+christopherwatkin@users.noreply.github.com>
2017-09-28 09:14:30 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,45 +1,69 @@
 class nginx {
-
-  File {
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
+  case $facts['os']['family'] {
+    'redhat','debian' : {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir = '/var/log/nginx'
+    }
+    'windows' : {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $logdir = 'C:/ProgramData/nginx/logs'
+    }
+    default : {
+      fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+    }
   }
 
-  $user_service_runs_as = $facts['os']['family'] ? {
+  # user the service will run as. Used in the nginx.conf.epp template
+  $user = $facts['os']['family'] ? {
     'redhat' => 'nginx',
     'debian' => 'www-data',
+    'windows' => 'nobody',
   }
 
-  package {'nginx':
+  File {
+    owner => $owner,
+    group => $group,
+    mode => '0664',
+  }
+
+  package { $package:
     ensure => present,
   }
-  
-  file {'/var/www':
-    ensure => 'directory',
-  }
-  
-  file {'/var/www/index.html':
-    ensure => 'file',
-    source => 'puppet:///modules/nginx/index.html',
-  }
-  
-  file { '/etc/nginx/nginx.conf':
-    ensure => file,
-    #source => 'puppet:///modules/nginx/nginx.conf',
-    content => epp(ngnix/nginx.conf.epp, {user_service_runs_as => $user_service_runs_as }),
-    require => Package['nginx'],
-    notify => Service['nginx'],
-  }
 
-  file { '/etc/nginx/conf.d':
+  file { [ $docroot, "${confdir}/conf.d" ]:
     ensure => directory,
   }
 
-  file { '/etc/nginx/conf.d/default.conf':
+  file { "${docroot}/index.html":
     ensure => file,
-    source => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
+    source => 'puppet:///modules/nginx/index.html',
+  }
+
+  file { "${confdir}/nginx.conf":
+    ensure => file,
+    content => epp('nginx/nginx.conf.epp',
+      {
+        user => $user,
+        confdir => $confdir,
+        logdir => $logdir,
+      }),
+    notify => Service['nginx'],
+  }
+
+  file { "${confdir}/conf.d/default.conf":
+    ensure => file,
+    content => epp('nginx/default.conf.epp',
+    {
+      docroot => $docroot,
+    }),
     notify => Service['nginx'],
   }
 
