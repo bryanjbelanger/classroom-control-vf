af805d6d68596e21a5e73bd127604dd79b548e2f
christopherwatkin <32306314+christopherwatkin@users.noreply.github.com>
2017-09-28 11:37:51 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,31 +1,23 @@
-class nginx (Optional[String] $root = undef,
-             Boolean $highperf = true,
-            ){
+class nginx {
   case $facts['os']['family'] {
     'redhat','debian' : {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      #$docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $logdir = '/var/log/nginx'
-      
-      # this will be used if we don't pass in a value
-      $default_docroot = '/var/www'
+        $package = 'nginx'
+        $owner = 'root'
+        $group = 'root'
+        $docroot = '/var/www'
+        $confdir = '/etc/nginx'
+        $logdir = '/var/log/nginx'
     }
     'windows' : {
-      $package = 'nginx-service'
-      $owner = 'Administrator'
-      $group = 'Administrators'
-      #$docroot = 'C:/ProgramData/nginx/html'
-      $confdir = 'C:/ProgramData/nginx'
-      $logdir = 'C:/ProgramData/nginx/logs'
-      
-      # this will be used if we don't pass in a value
-      $default_docroot = 'C:/ProgramData/nginx/html'
+        $package = 'nginx-service'
+        $owner = 'Administrator'
+        $group = 'Administrators'
+        $docroot = 'C:/ProgramData/nginx/html'
+        $confdir = 'C:/ProgramData/nginx'
+        $logdir = 'C:/ProgramData/nginx/logs'
     }
     default : {
-      fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+        fail("Module ${module_name} is not supported on ${facts['os']['family']}")
     }
   }
 
@@ -34,12 +26,6 @@ class nginx (Optional[String] $root = undef,
     'redhat' => 'nginx',
     'debian' => 'www-data',
     'windows' => 'nobody',
-  }
-
-  # if $root isn't set, then fall back to the platform default
-  $docroot = $root ? {
-    undef => $default_docroot,
-    default => $root,
   }
 
   File {
@@ -52,14 +38,13 @@ class nginx (Optional[String] $root = undef,
     ensure => present,
   }
 
-  # docroot is either passed in or a default value
-  nginx::vhost { 'default':
-    docroot => $docroot,
-    servername => $facts['fqdn'],
-  }
-  
-  file { "${docroot}/vhosts":
+  file { [ $docroot, "${confdir}/conf.d" ]:
     ensure => directory,
+  }
+
+  file { "${docroot}/index.html":
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
   }
 
   file { "${confdir}/nginx.conf":
@@ -67,13 +52,19 @@ class nginx (Optional[String] $root = undef,
     content => epp('nginx/nginx.conf.epp',
       {
         user => $user,
-        logdir => $logdir,
         confdir => $confdir,
-        blockdir => $blockdir,
-        highperf => $highperf,
+        logdir => $logdir,
       }),
-      require => Package[$package],
-      notify => Service['nginx'],
+    notify => Service['nginx'],
+  }
+
+  file { "${confdir}/conf.d/default.conf":
+    ensure => file,
+    content => epp('nginx/default.conf.epp',
+      {
+        docroot => $docroot,
+      }),
+    notify => Service['nginx'],
   }
 
   service { 'nginx':
