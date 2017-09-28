32ff8460d5e9e3af581ab63972f6f3e1a4e839f7
dpoppel <32306235+dpoppel@users.noreply.github.com>
2017-09-28 13:44:35 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,37 +1,40 @@
 class nginx (
-String $package = $nginx::params::package,
-String $owner = $nginx::params::owner,
-String $group = $nginx::params::group,
-String $docroot = $nginx::params::docroot,
-String $confdir = $nginx::params::confdir,
-String $blockdir = $nginx::params::blockdir,
-String $logdir = $nginx::params::logdir,
-String $user = $nginx::params::user,
-Boolean $highperf = $nginx::params::highperf,
+  String $package = $nginx::params::package,
+  String $owner = $nginx::params::owner,
+  String $group = $nginx::params::group,
+  String $docroot = $nginx::params::docroot,
+  String $confdir = $nginx::params::confdir,
+  String $blockdir = $nginx::params::blockdir,
+  String $logdir = $nginx::params::logdir,
+  String $user = $nginx::params::user,
+  Boolean $highperf = $nginx::params::highperf,
 ) inherits nginx::params {
-File {
-owner => $owner,
-group => $group,
-mode => '0664',
-}
-package { $package:
-ensure => present,
-}
-file { "${confdir}/nginx.conf":
-ensure => file,
-content => epp('nginx/nginx.conf.epp',
-{
-user => $user,
-logdir => $logdir,
-confdir => $confdir,
-blockdir => $blockdir,
-highperf => $highperf,
-}),
-require => Package[$package],
-notify => Service['nginx'],
-}
-service { 'nginx':
-ensure => running,
-enable => true,
-}
+  File {
+    owner => $owner,
+    group => $group,
+    mode => '0664',
+  }
+  
+  package { $package:
+    ensure => present,
+   }
+    
+  file { "${confdir}/nginx.conf":
+     ensure => file,
+    content => epp('nginx/nginx.conf.epp',
+        {
+          user => $user,
+          logdir => $logdir,
+          confdir => $confdir,
+          blockdir => $blockdir,
+          highperf => $highperf,
+        }),
+    require => Package[$package],
+    notify => Service['nginx'],
+  }
+  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
 }
