5b8c449c8c8deaefbba0c1d5d432dc2c76fa0334
ewestdal <32306206+ewestdal@users.noreply.github.com>
2017-09-28 11:08:58 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,20 +1,29 @@
-class nginx {
+class nginx (
+Optional[String] $root = undef,
+Boolean $highperf = true,
+) {
   case $facts['os']['family']{
     'redhat','debian' :{
       $package = 'nginx'
       $owner = 'root'
       $group = 'root'
-      $docroot = '/var/www'
+      #$docroot = '/var/www'
       $confdir = '/etc/nginx'
       $logdir = '/var/log/nginx'
+      
+      # this will be used if we don't pass in a value
+      $default_docroot = '/var/www'
     }
     'windows' : {
       $package = 'nginx-service'
       $owner = 'Administrator'
       $group = 'Administrators'
-      $docroot = 'C:/ProgramData/nginx/html'
+      #$docroot = 'C:/ProgramData/nginx/html'
       $confdir = 'C:/ProgramData/nginx'
       $logdir = 'C:/ProgramData/nginx/logs'
+
+      # this will be used if we don't pass in a value
+      $default_docroot = 'C:/ProgramData/nginx/html'
     }
     default : {
       fail("Module ${module_name} is not supported on ${facts['os']['family']}")
@@ -27,6 +36,12 @@ class nginx {
     'debian' => 'www-data',
     'windows' => 'nobody',
   }
+
+  # if $root isn't set, then fall back to the platform default
+  $docroot = $root ? {
+    undef => $default_docroot,
+    default => $root,
+  }
   
   File {
     owner => $owner,
@@ -37,6 +52,12 @@ class nginx {
   #ensure package is present
   package { $package:
     ensure => present,
+  }
+  
+  # docroot is either passed in or a default value
+  nginx::vhost { 'default':
+    docroot => $docroot,
+    servername => $facts['fqdn'],
   }
   
   # create a directory
