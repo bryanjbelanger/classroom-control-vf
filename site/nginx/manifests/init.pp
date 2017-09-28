68fab551f94fa1c287a0e1d0adbed1c4be20bbb1
dosweene <dosweene@gmail.com>
2017-09-28 11:05:17 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -7,13 +7,19 @@ class nginx (
     'redhat','debian' : { $package = 'nginx'
      $owner = 'root'
      $group = 'root'
-     $docroot = '/var/www' $confdir = '/etc/nginx' $logdir = '/var/log/nginx'
+     #$docroot = '/var/www' 
+     $confdir = '/etc/nginx' 
+     $logdir = '/var/log/nginx'
+     $default_docroot = '/var/www'
   }
   'windows' : {
      $package = 'nginx-service'
      $owner = 'Administrator'
      $group = 'Administrators'
-     $docroot = 'C:/ProgramData/nginx/html' $confdir = 'C:/ProgramData/nginx' $logdir = 'C:/ProgramData/nginx/logs'
+     #$docroot = 'C:/ProgramData/nginx/html' 
+     $confdir = 'C:/ProgramData/nginx' 
+     $logdir = 'C:/ProgramData/nginx/logs'
+     $default_docroot = 'C:/ProgramData/nginx/html'
   }
   default : {
       fail("Module ${module_name} is not supported on ${facts['os']['family']}") 
