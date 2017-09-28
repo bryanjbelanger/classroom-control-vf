16aaf87c5d08d47c6615d7ea5da7e0a3c070ae22
kcyr <32306311+kcyr@users.noreply.github.com>
2017-09-28 09:58:03 -0400

    Update mananged_user.pp

--- site/users/manifests/mananged_user.pp
+++ site/users/manifests/mananged_user.pp
@@ -1,12 +1,12 @@
 define users::managed_user (
-    $group = $title,
+$group = $title,
 ) {
-  user { $title:
-    ensure => present,
-  }
-  file { "/home/${title}":
-    ensure => directory,
-    owner => $title,
-    group => $group,
-  }
+user { $title:
+ensure => present,
+}
+file { "/home/${title}":
+ensure => directory,
+owner => $title,
+group => $group,
+}
 }
