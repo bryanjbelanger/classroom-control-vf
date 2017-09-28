a9db4b299c34d76f5d26208f0529fb1344d7e45f
Benjamin Ritter <britter@travelers.com>
2017-09-28 10:23:32 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,13 +43,5 @@ ini_setting { 'random ordering':
 
 node default {
   # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-  class profile::base {
-        # This is where you can declare classes for all nodes.
-        # Example:
-            # class { 'my_class': }
-        $message = lookup('message')
-        notify { $message: }
-}
+  # Example
 }
