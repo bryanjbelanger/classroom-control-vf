c9917aa969a814ac78a2b5eb63fff7be65e0ffd7
nsyphers <32306141+nsyphers@users.noreply.github.com>
2017-09-28 11:11:24 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -71,12 +71,6 @@ node default {
     root => '/var/www/html',
    }
 
-  node default {
-
-
-  }
-
-
   if $::virtual != 'physical' {
     $vmname = capitalize($::virtual)
     notify { "This is a ${vmname} virtual machine.": }
