5dc0e509f5e1d354c69c28cd70b8aada9b3d6367
Autostructure <bbelanger@azcender.com>
2017-09-28 13:36:20 -0400

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,25 +1,34 @@
 class nginx::params {
   case $facts['os']['family'] {
     'redhat','debian' : {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      # $docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $logdir = '/var/log/nginx'
-
-      # this will be used if we don't pass in a value
-      $default_docroot = '/var/www'
+      $package  = 'nginx'
+      $owner    = 'root'
+      $group    = 'root'
+      $docroot  = '/var/www'
+      $confdir  = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir   = '/var/log/nginx'
     }
     'windows' : {
-      $package = 'nginx-service'
-      $owner = 'Administrator'
-      $group = 'Administrators'
-      # $docroot = 'C:/ProgramData/nginx/html'
-      $confdir = 'C:/ProgramData/nginx'
-      $logdir = 'C:/ProgramData/nginx/logs'
-
-      # this will be used if we don't pass in a value
-      $default_docroot = 'C:/ProgramData/nginx/html'
+      $package  = 'nginx-service'
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+      $docroot  = 'C:/ProgramData/nginx/html'
+      $confdir  = 'C:/ProgramData/nginx'
+      $blockdir = 'C:/ProgramData/nginx/conf.d'
+      $logdir   = 'C:/ProgramData/nginx/logs'
     }
+    default : {
+      fail("Module ${module_name} is not supported on ${facts['os']['family']}") }
+    }
+  }
+    
+  # Default to high-performance mode
+  $highperf = true
+  
+  $user = $facts['os']['family'] ? {
+    'redhat' => 'nginx',
+    'debian' => 'www-data',
+    'windows' => 'nobody',
+  }
 }
