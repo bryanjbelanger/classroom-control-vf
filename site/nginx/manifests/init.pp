3eecb702118189d154bf29a075f39771fd95ba53
dosweene <dosweene@gmail.com>
2017-09-28 13:34:50 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,35 +1,7 @@
-#Docu
 class nginx(
   Optional[String] $root = undef,
-) {
-  case $facts['os']['family'] {
-    'redhat','debian' : {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      # $docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $logdir = '/var/log/nginx'
-
-      # this will be used if we don't pass in a value
-      $default_docroot = '/var/www'
-    }
-    'windows' : {
-      $package = 'nginx-service'
-      $owner = 'Administrator'
-      $group = 'Administrators'
-      # $docroot = 'C:/ProgramData/nginx/html'
-      $confdir = 'C:/ProgramData/nginx'
-      $logdir = 'C:/ProgramData/nginx/logs'
-
-      # this will be used if we don't pass in a value
-      $default_docroot = 'C:/ProgramData/nginx/html'
-    }
-    default : {
-      fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-    }
-  }
-
+) inherits nginx::params {
+ 
   # if $root isn't set, then fall back to the platform default
   $docroot = $root ? {
     undef   => $default_docroot,
