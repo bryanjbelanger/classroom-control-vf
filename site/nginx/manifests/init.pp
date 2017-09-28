64217f9eda3d4f94aec5d7479f07a5840bab2d1c
Autostructure <bbelanger@azcender.com>
2017-09-28 12:17:59 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -26,7 +26,7 @@ class harden_docker::nginx(
       $default_docroot = 'C:/ProgramData/nginx/html'
     }
     default : {
-      fail("Module ${module_name} is not supported on ${facts['os']['family']}") }
+      fail("Module ${module_name} is not supported on ${facts['os']['family']}")
     }
   }
 
@@ -35,33 +35,27 @@ class harden_docker::nginx(
     undef   => $default_docroot,
     default => $root,
   }
-
   # user the service will run as. Used in the nginx.conf.epp template
   $user = $facts['os']['family'] ? {
     'redhat'  => 'nginx',
     'debian'  => 'www-data',
     'windows' => 'nobody',
   }
-
   File {
     owner => $owner,
     group => $group,
     mode  => '0664',
   }
-
   package { $package:
     ensure => present,
   }
-
   file { [ $docroot, "${confdir}/conf.d" ]:
     ensure => directory,
   }
-
   file { "${docroot}/index.html":
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
-
   file { "${confdir}/nginx.conf":
     ensure  => file,
     content => epp('nginx/nginx.conf.epp',
@@ -73,7 +67,6 @@ class harden_docker::nginx(
     ),
     notify  => Service['nginx'],
   }
-
   file { "${confdir}/conf.d/default.conf":
     ensure  => file,
     content => epp('nginx/default.conf.epp',
