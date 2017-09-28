f999809793010e9e668296450e8bb9b187ebfc39
dpoppel <32306235+dpoppel@users.noreply.github.com>
2017-09-28 13:33:42 -0400

    Create params.pp

---
+++ site/nginx/manifests/params.pp
@@ -0,0 +1,32 @@
+class nginx::params {
+case $facts['os']['family'] {
+'redhat','debian' : {
+$package = 'nginx'
+$owner = 'root'
+$group = 'root'
+$docroot = '/var/www'
+$confdir = '/etc/nginx'
+$blockdir = '/etc/nginx/conf.d'
+$logdir = '/var/log/nginx'
+}
+'windows' : {
+$package = 'nginx-service'
+$owner = 'Administrator'
+$group = 'Administrators'
+$docroot = 'C:/ProgramData/nginx/html'
+$confdir = 'C:/ProgramData/nginx'
+$blockdir = 'C:/ProgramData/nginx/conf.d'
+$logdir = 'C:/ProgramData/nginx/logs'
+}
+default : {
+fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+}
+}
+# Default to high-performance mode
+$highperf = true
+$user = $facts['os']['family'] ? {
+'redhat' => 'nginx',
+'debian' => 'www-data',
+'windows' => 'nobody',
+}
+}
