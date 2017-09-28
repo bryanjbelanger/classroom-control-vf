3f7e6b7ad9a3ef337cb1507d9851cf786d7e1f84
JohnnyB-CT <32306220+JohnnyB-CT@users.noreply.github.com>
2017-09-28 13:32:10 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,6 +46,7 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   include ::users
+  include ngix
   if $facts['manufacturer'] == 'Xen' {
     notify {"This is a vritual machine": }
   }
