fe20353827da38f6ddc548354c6aa7dd20f14a5e
JohnnyB-CT <32306220+JohnnyB-CT@users.noreply.github.com>
2017-09-28 11:21:59 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,55 +1,77 @@
 class nginx (
-Optional[String] $root = undef, Boolean $highperf = true,
+Optional[String] $root = undef,
+Boolean $highperf = true,
 ){
 case $facts['os']['family'] {
 'redhat','debian' : {
-$package
-$owner
-$group
-$docroot
-$confdir
-$blockdir = '/etc/nginx/conf.d' $logdir = '/var/log/nginx'
+$package = 'nginx'
+$owner = 'root'
+$group = 'root'
+$docroot = '/var/www'
+$confdir '/etc/nginx'
+$blockdir = '/etc/nginx/conf.d'
+$logdir = '/var/log/nginx'
 # this will be used if we don't pass in a value
 $default_docroot = '/var/www' }
 'windows' : {
-$package $owner $group $docroot $confdir $blockdir = $logdir =
-# this will
-'nginx-service' 'Administrator' 'Administrators' 'C:/ProgramData/nginx/html' 'C:/ProgramData/nginx' 'C:/ProgramData/nginx/conf.d' 'C:/ProgramData/nginx/logs'
-be used if we don't pass in a value
-= = = = =
-$default_docroot = 'C:/ProgramData/nginx/html' }
+$package = 'nginx-service'
+$owner = 'Administrator'
+$group = 'Administrators'
+# $docroot = 'C:/ProgramData/nginx/html'
+$confdir = 'C:/ProgramData/nginx'
+$blockdir = 'C:/ProgramData/nginx/conf.d'
+$logdir = 'C:/ProgramData/nginx/logs'
+# this will be used if we don't pass in a value
+$default_docroot = 'C:/ProgramData/nginx/html' 
+}
 default : {
 fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-} }
+}
+}
 # user the service will run as. Used in the nginx.conf.epp template
-$user = $facts['os']['family'] ? { 'redhat' => 'nginx',
-'debian' => 'www-data', 'windows' => 'nobody',
+$user = $facts['os']['family'] ? { 
+'redhat' => 'nginx',
+'debian' => 'www-data', 
+'windows' => 'nobody',
 # if $root isn't set, then fall back to the platform default
 $docroot = $root ? {
-undef => $default_docroot, default => $root,
+undef => $default_docroot, 
+default => $root,
 File {
-owner => $owner, group => $group, mode => '0664',
+owner => $owner, 
+group => $group, 
+mode => '0664',
 }
-package { $package: ensure => present,
-}
-
-# docroot is nginx::vhost
-docroot servername
-either passed in or a default value { 'default':
-=> $docroot,
-=> $facts['fqdn'],
+package { $package: 
+ensure => present,
 }
 
-file { "${docroot}/vhosts": ensure => directory,
+# docroot is either passed in or a default value
+nginx::vhost { 'default':
+docroot => $docroot,
+servername => $facts['fqdn'],
+}
+
+file { "${docroot}/vhosts": 
+ensure => directory,
 }
 
 file { "${confdir}/nginx.conf":
 ensure => file,
 content => epp('nginx/nginx.conf.epp',
 {
+user => $user,
+logdir => $logdir,
+confdir => $confdir,
+blockdir => $blockdir,
+highperf => $highperf,
+}),
 require => Package[$package],
-notify => Service['nginx'], }
-
-service { 'nginx': ensure => running, enable => true,
-} }
+notify => Service['nginx'],
+}
+service { 'nginx':
+ensure => running,
+enable => true,
+}
+}
 
